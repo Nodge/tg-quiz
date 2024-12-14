7553870084c9dd7031da7d0e0f7d58db86c181e9
Maxim Zemskov <nodge@yandex.ru>
2024-12-14 18:11:12 +0100

    fix texts

--- src/api/api-leaderboard.ts
+++ src/api/api-leaderboard.ts
@@ -1,9 +1,11 @@
 import { Answer } from '../model/Answer';
 import { AnswerRepository } from '../model/AnswerRepository';
+import { User } from '../model/User';
 import { UserRepository } from '../model/UserRepository';
 import { apiHandler } from '../shared/api-handler';
 
 export interface LeaderBoardItem {
+    userId: string;
     name: string;
     avatarUrl: string | null;
     score: number;
@@ -18,17 +20,25 @@ export const handler = apiHandler(async () => {
     const usersData = await users.getAllUsers();
     const answersData = await answers.getAllAnswers();
     const scoreMap = getScoreMap(answersData);
+    const createdAtMap = getRegisterDateMap(usersData);
 
     for (const user of usersData) {
         data.push({
+            userId: user.telegramId,
             name: user.telegramLogin,
             avatarUrl: user.telegramAvatarUrl,
             score: scoreMap.get(user.telegramId) ?? 0,
         });
     }
 
+    const now = Date.now();
     data.sort((a, b) => {
-        return b.score - a.score;
+        const diff = b.score - a.score;
+        if (diff !== 0) {
+            return diff;
+        }
+
+        return (createdAtMap.get(a.userId) ?? now) - (createdAtMap.get(b.userId) ?? now);
     });
 
     return {
@@ -46,6 +56,16 @@ function getScoreMap(answers: Answer[]) {
         let total = map.get(userId) ?? 0;
         total += score;
         map.set(userId, total);
+    }
+
+    return map;
+}
+
+function getRegisterDateMap(users: User[]) {
+    const map = new Map<string, number>();
+
+    for (const item of users) {
+        map.set(item.telegramId, item.createdAt);
     }
 
     return map;
