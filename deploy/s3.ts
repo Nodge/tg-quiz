3d337556580ea05477f80fa39381ecea6a65439d
Maxim Zemskov <nodge@yandex.ru>
2024-12-16 17:42:32 +0100

    remove public access to the bucket

--- deploy/s3.ts
+++ deploy/s3.ts
@@ -1,13 +1,12 @@
 export const avatarsBucket = new sst.aws.Bucket('Avatars', {
-    public: true,
+    access: 'cloudfront',
 });
-
-const region = aws.getRegionOutput().name;
-const s3Host = $interpolate`${avatarsBucket.name}.s3.${region}.amazonaws.com`;
 
 const router = new sst.aws.Router('AvatarsCDN', {
     routes: {
-        '/*': $interpolate`https://${s3Host}`,
+        '/*': {
+            bucket: avatarsBucket,
+        },
     },
 });
 
