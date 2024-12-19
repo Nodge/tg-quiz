260775d80333665118056840d697c9af86e9048c
Maxim Zemskov <nodge@yandex.ru>
2024-12-19 20:35:53 +0100

    fix api exports

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,9 +1,10 @@
-export type { CurrentQuestionResponse } from './handlers/current-question';
-export type { DeleteQuestionRequest } from './handlers/delete-question';
-export type { QuestionsResponse } from './handlers/get-questions';
+export type { ResetResponse } from './handlers/admin/reset';
+
+export type { CurrentQuestionResponse } from './handlers/admin/current-question';
+export type { DeleteQuestionRequest } from './handlers/admin/delete-question';
+export type { QuestionsResponse } from './handlers/admin/get-questions';
 export type { LeaderBoardResponse } from './handlers/leaderboard';
-export type { ResetResponse } from './handlers/reset';
-export type { SaveQuestionsOrderRequest } from './handlers/save-order';
-export type { SaveQuestionRequest } from './handlers/save-question';
-export type { NextQuestionResponse } from './handlers/start-question';
-export type { StopQuestionRequest, StopQuestionResponse } from './handlers/stop-question';
+export type { SaveQuestionsOrderRequest } from './handlers/admin/save-order';
+export type { SaveQuestionRequest } from './handlers/admin/save-question';
+export type { NextQuestionResponse } from './handlers/admin/start-question';
+export type { StopQuestionRequest, StopQuestionResponse } from './handlers/admin/stop-question';
